338e8cc549c42a6fadacc5df1d845f4df9e8c51b
zar1na <zarinaaax@icloud.com>
2024-04-14 22:04:57 +0100

    previous correct guesses now pre-fill letters in word

--- index.php
+++ index.php
@@ -54,6 +54,7 @@ $stage = [1=>"
 
 $attempt = 1;
 $word = str_split(getWord());
+$prevguess = array();
 
 echo "Welcome to Hangman \n";
 for ($i=0;$i<count($stage); $i++) {
@@ -61,9 +62,7 @@ for ($i=0;$i<count($stage); $i++) {
     $guess = readline('Guess:');
     echo "\n";
     for ($x=0; $x<count($word); $x++) {
-
         checkUsrInput($word,$guess,$stage,$attempt,$x);
-        
     }
     echo "\n\n";
 }
@@ -81,7 +80,10 @@ function getWord() {
 function checkUsrInput($word,$guess,$stage,$attempt,$x) {
     # checks whether the letter inputted is in the word ramdomly chosen. If it is then it will print out the letters within the string. Otherwise, the stage will increment and the user will have to guess another letter
 
-    if ($word[$x] == $guess) {
+    global $prevguess;
+
+    if (in_array($word[$x], $prevguess) || $word[$x] == $guess) {
+        $prevguess[] .= $guess;
         echo " ".$word[$x]." ";
     }
     else {
@@ -89,37 +91,6 @@ function checkUsrInput($word,$guess,$stage,$attempt,$x) {
         incrementStage($attempt);
         // print_r($stage[$attempt])."\n";
     }
-
-    // $stage = $stage[$attempt];
-    // echo $stage."\n";
-    // incrementStage($attempt);
-
-    // if (str_contains(strtolower(getWord()), strtolower($guess))) {
-    //   echo "Yes \n";
-    //   $word = str_split(getWord());
-    //   // print_r($word);
-    //   // echo count($word);
-    // }
-    // else {
-    //   echo "No \n";
-    //   $stage = $stage[$attempt];
-    //   echo $stage."\n";
-    //   $attempt++;
-    // }
-
-    // if (str_contains(strtolower(getWord()), strtolower($guess))) {
-    // echo "Yes \n";
-    // $word = str_split(getWord());
-    // // print_r($word);
-    // // echo count($word);
-    // }
-    // else {
-    // echo "No \n";
-    // $stage = $stage[$attempt];
-    // echo $stage."\n";
-    // $attempt++;
-    // }
-
 }
 
 function incrementStage() {
