84ce79e1008af8c73ffaf5852a0c1bf7a376c5fa
Zarina <56252407+zar1na@users.noreply.github.com>
2024-04-30 22:32:22 +0100

    3 add logic to check userinput (#4)
    * added functionality to loop through word length and print out
    corresponding guess
    
    * previous correct guesses now pre-fill letters in word
    
    * reworked how prevguess appends and guess is lowercase
    
    * the game is playable.. now onto adding the different stages
    
    * Test cheese has been removed and functionality tested
    
    * fixed bad ending word reveal
    
    * stages implemented

--- index.php
+++ index.php
@@ -1,80 +1,123 @@
 <?php
 
-$stages = [1=>"
-  +---+
-  |   |
-      |
-      |
-      |
-      |
+$stage = [1=>"
++---+
+|   |
+|
+|
+|
+|
 =========",2=>"
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
++---+
+|   |
+|   O
+|
+|
+|
 =========",3=>"
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
++---+
+|   |
+|   O
+|   |
+|
+|
 =========",4=>"
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
++---+
+|   |
+|   O
+|  /|
+|
+|
 =========",5=>"
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
++---+
+|   |
+|   O
+|  /|\
+|
+|
 =========",6=>"
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
++---+
+|   |
+|   O
+|  /|\
+|  /
+|
 =========",7=>"
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
++---+
+|   |
+|   O
+|  /|\
+|  / \
+|
 ========="
 ];
 
-$guess = readline('Guess a letter: ');
+$word = getWord();
+$lowword = str_split(strtolower($word));
+$prevguess = array();
+$badattempts = 1;
 
-$attempts = 0;
+echo "Welcome to Hangman \n";
+echo "Your word to guess is.. \n";
+for ($i=0;$i<count($lowword);$i++) {
+    echo " _ ";
+}
+echo "\n\n";
 
-checkUsrInput($guess,$stages,$attempts);
+while ($badattempts < count($stage)) {
 
-// getWord();
-// echo getWord()."\n";
+    if (count(array_unique($prevguess)) == count(array_unique($lowword))) {
+        echo "You won! \n\nRun the program to play again!\n";
+        exit;
+    }
+    else {
+        $guess = strtolower(readline('Guess:'));
+        echo "\n";
+
+        checkUsrInput($lowword,$guess,$stage,$badattempts);
+
+        echo "\n\n";
+    }
+}
+
+echo "Your word was... ".$word."\n\nRun the program to play again!\n";
 
 function getWord() {
-  $words = ['Chocolate','Cheese','Test','Paper','Shark','Empire','Toss','Jumper','Pain ','Bottle','Pat','Love','Song','Back','Muscles','Sighing','Beast','Bed','Lie'];
+    $words = ['Chocolate','Cheese','Test','Paper','Shark','Empire','Toss','Jumper','Pain ','Bottle','Pat','Love','Song','Back','Muscles','Sighing','Beast','Bed','Lie'];
 
-  $ran = rand(0, count($words)-1);
-  return $words[$ran];
+    $ran = rand(0, count($words)-1);
+    return $words[$ran];
 }
 
-function checkUsrInput($guess,$stages,$attempts) {
-  # checks whether the letter inputted is in the word ramdomly chosen. If it is then it will print out the letters within the string. Otherwise, the stage will increment and the user will have to guess another letter
+function checkUsrInput($lowword,$guess,$stage,$badattempts) {
+    # checks whether the letter inputted is in the word ramdomly chosen. If it is then it will print out the letters within the string. Otherwise, the stage will increment and the user will have to guess another letter
+
+    global $prevguess;
+    global $badattempts;
+
+    if (!in_array($guess, $lowword)) {
+        $badattempts++;
+        print_r($stage[$badattempts]);
+        echo "\n";
+    }
+
+    for ($x=0; $x<count($lowword); $x++) {
+        
+        if (in_array($lowword[$x], $prevguess) || $lowword[$x] == $guess) {
+            if ($lowword[$x] == $guess) {
+                $prevguess[] .= $guess;
+            }
+            echo " ".$lowword[$x]." ";
+        }
+        else {
+            echo " _ ";
+        }
+    }
 }
 
-function incrementStage($attempts) {
+function incrementStage($stage) {
     # goes to the next stage of the game
-    $attempts++;
+    $stage++;
 
-    return $attempts;
+    return $stage;
 }
