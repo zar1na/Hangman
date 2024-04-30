89b2a7214fa38ef91e62a2fa5511ec6fe4143274
zar1na <zarinaaax@icloud.com>
2024-04-30 15:53:51 +0100

    Test cheese has been removed and functionality tested

--- index.php
+++ index.php
@@ -52,19 +52,20 @@ $stage = [1=>"
 ========="
 ];
 
-$word = str_split(strtolower(getWord()));
+$word = getWord();
+$lowword = str_split(strtolower($word));
 $prevguess = array();
 
 echo "Welcome to Hangman \n";
 echo "Your word to guess is.. \n";
-for ($i=0;$i<count($word);$i++) {
+for ($i=0;$i<count($lowword);$i++) {
     echo " _ ";
 }
 echo "\n\n";
 
 for ($counter=1;$counter<=count($stage); $counter++) {
 
-    if (count(array_unique($prevguess)) == count(array_unique($word))) {
+    if (count(array_unique($prevguess)) == count(array_unique($lowword))) {
         echo "You won! \n\nRun the program to play again!\n";
         exit;
     }
@@ -72,33 +73,33 @@ for ($counter=1;$counter<=count($stage); $counter++) {
         $guess = strtolower(readline('Guess:'));
         echo "\n";
 
-        checkUsrInput($word,$guess,$stage,$counter);
+        checkUsrInput($lowword,$guess,$stage,$counter);
 
         echo "\n\n";
     }
 }
 
-echo "Your word was... ".getWord()."\n\nRun the program to play again!\n";
+echo "Your word was... ".$word."\n\nRun the program to play again!\n";
 
 function getWord() {
     $words = ['Chocolate','Cheese','Test','Paper','Shark','Empire','Toss','Jumper','Pain ','Bottle','Pat','Love','Song','Back','Muscles','Sighing','Beast','Bed','Lie'];
 
     $ran = rand(0, count($words)-1);
-    // return $words[$ran];
-    return $words[1]; // cheese for testing purposes
+    return $words[$ran];
+    // return $words[1]; // cheese for testing purposes
 }
 
-function checkUsrInput($word,$guess,$stage,$counter) {
+function checkUsrInput($lowword,$guess,$stage,$counter) {
     # checks whether the letter inputted is in the word ramdomly chosen. If it is then it will print out the letters within the string. Otherwise, the stage will increment and the user will have to guess another letter
 
     global $prevguess;
     
-    for ($x=0; $x<count($word); $x++) {
-        if (in_array($word[$x], $prevguess) || $word[$x] == $guess) {
-            if ($word[$x] == $guess) {
+    for ($x=0; $x<count($lowword); $x++) {
+        if (in_array($lowword[$x], $prevguess) || $lowword[$x] == $guess) {
+            if ($lowword[$x] == $guess) {
                 $prevguess[] .= $guess;
             }
-            echo " ".$word[$x]." ";
+            echo " ".$lowword[$x]." ";
         }
         else {
             echo " _ ";
